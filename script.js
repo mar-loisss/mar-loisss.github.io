b11b74acafe597879ffc568a58422cfbf34c9aad
mar-loisss <louise.credo@btths.edu.ph>
2025-11-28 19:19:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".card__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
